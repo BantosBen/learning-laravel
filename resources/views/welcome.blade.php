cd1ebc6c0714fbc7fa8a7cdc75cbdbbae9f4a5a3
Angatia Benson <angatiabenson1@gmail.con>
2023-05-09 05:41:26 -0500

    feat: view layouts, passing data into layouts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,24 +1,13 @@
-<!DOCTYPE html>
-<html>
+@extends('app')
 
-<head>
-    <title>Welcome</title>
-</head>
-
-<body>
-    <h1>Welcome to my website My name is {{ $name }}</h1>
-
-
+@section('content')
     @if (count($people) > 0)
         <ol>
             @foreach ($people as $person)
                 <li>{{ $person }}</li>
             @endforeach
-            </ul>
-        @else
-            <p>No People</p>
+        </ol>
+    @else
+        <p>No People</p>
     @endif
-    </ol>
-</body>
-
-</html>
+@endsection
